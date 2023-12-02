3e85eab0d11bfbe2ee878ccfd9fdbc6fcdb5c99f
marun <marun@DESKTOP-2P9MSI7>
2023-12-02 14:39:53 +0530

    Add project files.

---
+++ WebApplication4/Default.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication4._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  <div>
+      <div style="font-size:x-large" align="center">Job Fair Form<table class="w-100">
+          <tr>
+              <td style="width: 102px">&nbsp;</td>
+              <td style="width: 113px">Name</td>
+              <td>
+                  <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                  <asp:RequiredFieldValidator ID="rfvName" runat="server" ControlToValidate="TextBox1"
+    ErrorMessage="Name is required." ValidationGroup="validationGroup"></asp:RequiredFieldValidator>
+              </td>
+          </tr>
+          <tr>
+              <td style="width: 102px; height: 31px"></td>
+              <td style="width: 113px; height: 31px">Gender</td>
+              <td style="height: 31px">
+                 
+
+                  <asp:RadioButtonList ID="radioButtonListOptions" runat="server">
+    <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+    <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+</asp:RadioButtonList>
+
+<asp:RequiredFieldValidator ID="requiredFieldValidatorOptions" runat="server" 
+    ControlToValidate="radioButtonListOptions" ErrorMessage="Please select an gender"
+    InitialValue="0" Display="Dynamic">
+</asp:RequiredFieldValidator>
+
+
+
+              </td>
+          </tr>
+          <tr>
+              <td style="width: 102px">&nbsp;</td>
+              <td style="width: 113px">Email</td>
+              <td>
+                  <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                  <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox2"
+    ErrorMessage="Email is required." ValidationGroup="validationGroup"></asp:RequiredFieldValidator>
+                   <asp:RegularExpressionValidator ID="revEmail" runat="server" ControlToValidate="TextBox2"
+                ErrorMessage="Invalid email format" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"
+                Display="Dynamic"></asp:RegularExpressionValidator>
+              </td>
+          </tr>
+          <tr>
+              <td style="width: 102px">&nbsp;</td>
+              <td style="width: 113px">&nbsp;</td>
+              <td>
+                  <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Clear" Width="87px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td>
+          </tr>
+          <tr>
+              <td style="width: 102px">&nbsp;</td>
+              <td style="width: 113px">&nbsp;</td>
+              <td>
+                  <asp:GridView ID="GridView1" runat="server">
+                  </asp:GridView>
+              </td>
+          </tr>
+          </table>
+      </div>
+      <br/>
+  </div>
+
+</asp:Content>
---
+++ WebApplication4/Default.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class _Default : Page
+    {
+        string name = "";
+        string connections = "Server=DESKTOP-2P9MSI7\\SQLEXPRESS;Database=Database Design;Integrated Security = true;";
+        string gender="";
+        string email= "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            name = TextBox1.Text;
+        
+             email= TextBox2.Text;
+            
+            gender = radioButtonListOptions.SelectedValue;
+            try
+            {
+                SqlConnection connection = new SqlConnection(connections);
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO JobFairEntries(name,gender,email) VALUES(@name,@gender,@email)";
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@gender", gender);
+                        command.Parameters.AddWithValue("@email", email);
+                        command.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+                Response.Write("Data inserted into database successfully!");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error:" + ex.Message);
+
+            }
+            BindGrid();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            radioButtonListOptions.SelectedIndex = -1;
+            
+        }
+        protected void BindGrid()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connections))
+                {
+                    connection.Open();
+
+                    string selectQuery = "SELECT * FROM JobFairEntries ";
+
+                    using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                    {
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        dataAdapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            GridView1.DataSource = dataTable;
+                            GridView1.DataBind();
+                        }
+                        else
+                        {
+                            // No data found
+                            GridView1.DataSource = null;
+                            GridView1.DataBind();
+                            Response.Write("No data found in the database.");
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+    
+    }
+}
